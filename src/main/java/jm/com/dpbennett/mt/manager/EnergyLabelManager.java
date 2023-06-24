19511f58b7f8008cc6f464abf89bbdfc253c763c
Desmond Bennett <Desmond Bennett@DESKTOP-H30NB01>
2023-06-24 17:01:26 -0500

    General updates.

---
+++ src/main/java/jm/com/dpbennett/mt/manager/EnergyLabelManager.java
@@ -0,0 +1,305 @@
+/*
+LabelPrint 
+Copyright (C) 2023  D P Bennett & Associates Limited
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Email: info@dpbennett.com.jm
+ */
+package jm.com.dpbennett.mt.manager;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.faces.model.SelectItem;
+import javax.persistence.EntityManager;
+import jm.com.dpbennett.business.entity.mt.EnergyLabel;
+import jm.com.dpbennett.business.entity.rm.DatePeriod;
+import jm.com.dpbennett.business.entity.sm.SystemOption;
+import jm.com.dpbennett.business.entity.util.BusinessEntityUtils;
+import jm.com.dpbennett.cm.manager.ClientManager;
+import jm.com.dpbennett.hrm.manager.HumanResourceManager;
+import jm.com.dpbennett.rm.manager.ReportManager;
+import jm.com.dpbennett.sm.manager.GeneralManager;
+import jm.com.dpbennett.sm.manager.SystemManager;
+import jm.com.dpbennett.sm.util.BeanUtils;
+import jm.com.dpbennett.sm.util.MainTabView;
+import jm.com.dpbennett.sm.util.PrimeFacesUtils;
+import org.primefaces.PrimeFaces;
+import org.primefaces.event.CellEditEvent;
+
+/**
+ *
+ * @author Desmond Bennett
+ */
+public class EnergyLabelManager extends GeneralManager
+        implements Serializable {
+
+    private String searchText;
+    private List<EnergyLabel> foundEnergyLabels;
+    private EnergyLabel selectedEnergyLabel;
+
+    /**
+     * Creates a new instance of LabelManager
+     */
+    public EnergyLabelManager() {
+        init();
+    }
+
+    public List<SelectItem> getEnergyEfficiencyProductTypes() {
+        return SystemManager.getStringListAsSelectItems(getEntityManager1(),
+                "energyEfficiencyProductTypes");
+    }
+
+    public List<SelectItem> getDefrostTypes() {
+        return SystemManager.getStringListAsSelectItems(getEntityManager1(),
+                "defrostTypes");
+    }
+
+    public List<SelectItem> getRefrigeratorFeatures() {
+        return SystemManager.getStringListAsSelectItems(getEntityManager1(),
+                "refrigeratorFeatures");
+    }
+    
+    public List<SelectItem> getRatedVoltages() {
+        return SystemManager.getStringListAsSelectItems(getEntityManager1(),
+                "ratedVoltages");
+    }
+    
+    public List<SelectItem> getRatedFrequencies() {
+        return SystemManager.getStringListAsSelectItems(getEntityManager1(),
+                "ratedFrequencies");
+    }
+    
+    public List<SelectItem> getEnergyEfficiencyClasses() {
+        return SystemManager.getStringListAsSelectItems(getEntityManager1(),
+                "energyEfficiencyClasses");
+    }
+
+    public void okLabel() {
+        if (selectedEnergyLabel.getIsDirty()) {
+            selectedEnergyLabel.save(getEntityManager1());
+            selectedEnergyLabel.setIsDirty(false);
+        }
+
+        PrimeFaces.current().dialog().closeDynamic(null);
+    }
+
+    public void cancelLabelEdit() {
+        PrimeFaces.current().dialog().closeDynamic(null);
+    }
+
+    public void updateEnergyLabel() {
+        selectedEnergyLabel.setIsDirty(true);
+    }
+
+    public EnergyLabel getSelectedEnergyLabel() {
+        return selectedEnergyLabel;
+    }
+
+    public void setSelectedEnergyLabel(EnergyLabel selectedEnergyLabel) {
+        this.selectedEnergyLabel = selectedEnergyLabel;
+    }
+
+    public void editSelectedEnergyLabel() {
+
+        PrimeFacesUtils.openDialog(null, "labelDialog", true, true, true, 600, 700);
+    }
+
+    public void createNewEnergyLabel() {
+        selectedEnergyLabel = new EnergyLabel();
+
+        editSelectedEnergyLabel();
+    }
+
+    @Override
+    public String getApplicationHeader() {
+
+        return "LabelPrint";
+    }
+
+    /**
+     * Gets the SystemManager object as a session bean.
+     *
+     * @return
+     */
+    public SystemManager getSystemManager() {
+        return BeanUtils.findBean("systemManager");
+    }
+    
+    @Override
+    public String getAppShortcutIconURL() {
+        return (String) SystemOption.getOptionValueObject(
+                getEntityManager1(), "appShortcutIconURL");
+    }
+
+    @Override
+    public MainTabView getMainTabView() {
+        return getSystemManager().getMainTabView();
+    }
+
+    @Override
+    public final void init() {
+        reset();
+    }
+    
+    public ReportManager getReportManager() {
+        return BeanUtils.findBean("reportManager");
+    }
+    
+    public ClientManager getClientManager() {
+
+        return BeanUtils.findBean("clientManager");
+    }
+
+    public HumanResourceManager getHumanResourceManager() {
+        return BeanUtils.findBean("humanResourceManager");
+    }
+
+    private void initManagers() {
+        try {
+
+            getClientManager();
+
+        } catch (Exception e) {
+            System.out.println("An error occured while resetting managers: " + e);
+        }
+    }
+
+    @Override
+    public void reset() {
+       super.reset();
+
+        setSearchType("Energy labels");
+        setSearchText("");
+        setDefaultCommandTarget("doSearch");
+        setModuleNames(new String[]{
+            "energyLabelManager",
+            "clientManager",
+            "reportManager"
+        });
+        setDateSearchPeriod(new DatePeriod("This month", "month",
+                "dateAndTimeEntered", null, null, null, false, false, false));
+        getDateSearchPeriod().initDatePeriod();
+
+    }
+    
+    @Override
+    public void doDefaultSearch(
+            MainTabView mainTabView,
+            String dateSearchField,
+            String searchType,
+            String searchText,
+            Date startDate,
+            Date endDate) {
+
+        switch (searchType) {
+            case "Energy labels":         
+                //findLabels();
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    // tk Implement find* method in the EnergyLabel class that searches more fields.
+    public List<EnergyLabel> findLabels(String searchField,
+            String searchPattern) {
+
+        List<EnergyLabel> labelsFound;
+
+        String query = "SELECT r FROM EnergyLabel r WHERE r." + searchField + " LIKE '%" + searchPattern + "%'";
+
+        try {
+            labelsFound = (List<EnergyLabel>) getEntityManager1().createQuery(query).getResultList();
+        } catch (Exception e) {
+            System.out.println(e);
+
+            labelsFound = new ArrayList<>();
+        }
+
+        return labelsFound;
+    }
+
+    public List<EnergyLabel> getFoundEnergyLabels() {
+        if (foundEnergyLabels == null) {
+            foundEnergyLabels = findLabels("model", "");
+        }
+        return foundEnergyLabels;
+    }
+
+    public void setFoundEnergyLabels(List<EnergyLabel> foundEnergyLabels) {
+        this.foundEnergyLabels = foundEnergyLabels;
+    }
+
+    public void onLabelCellEdit(CellEditEvent event) {
+        BusinessEntityUtils.saveBusinessEntityInTransaction(getEntityManager1(),
+                getFoundEnergyLabels().get(event.getRowIndex()));
+    }
+
+    public int getNumLabelsFound() {
+        return getFoundEnergyLabels().size();
+    }
+
+    public void doEnergyLabelSearch() {
+        // tk Implement better search
+        foundEnergyLabels = findLabels("brand", searchText);
+    }
+
+    public String getSearchText() {
+        return searchText;
+    }
+
+    public void setSearchText(String searchText) {
+        this.searchText = searchText;
+    }
+
+    @Override
+    public EntityManager getEntityManager1() {
+
+        return getSystemManager().getEntityManager1();
+
+    }
+
+    @Override
+    public EntityManager getEntityManager2() {
+
+        return getSystemManager().getEntityManager2();
+
+    }
+
+    public void doDefaultSearch() {
+
+        switch (getSystemManager().getDashboard().getSelectedTabId()) {
+            case "Energy Labels":
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void completeLogin() {
+        initDashboard();
+        initMainTabView();
+    }
+
+    @Override
+    public void completeLogout() {
+        reset();
+    }
+
+}
