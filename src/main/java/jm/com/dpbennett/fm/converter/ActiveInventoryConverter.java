7ef9b5100105c2fd1b60fdb760d911b71d2b3bf6
Desmond Bennett <desbenn@yahoo.com>
2024-09-18 19:35:59 -0500

    General updates.

--- src/main/java/jm/com/dpbennett/fm/converter/ActiveInventoryConverter.java
+++ src/main/java/jm/com/dpbennett/fm/converter/ActiveInventoryConverter.java
@@ -23,7 +23,7 @@ import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.FacesConverter;
 import javax.persistence.EntityManager;
-import jm.com.dpbennett.business.entity.fm.Inventory;
+import jm.com.dpbennett.business.entity.im.Inventory;
 import jm.com.dpbennett.sm.converter.ConverterAdapter;
 
 /**
