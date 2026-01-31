727fd404eea7225bd2fde5149fefa695691f8670
Desmond Bennett <desbenn@yahoo.com>
2026-01-31 13:15:22 -0500

    General updates.

--- src/test/java/jm/com/dpbennett/fm/stripe/ProductAPI.java
+++ src/test/java/jm/com/dpbennett/fm/stripe/ProductAPI.java
@@ -22,8 +22,6 @@ package jm.com.dpbennett.fm.stripe;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import org.junit.Test;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.stripe.model.Product;
 
 /**
@@ -34,14 +32,12 @@ public class ProductAPI {
 
   
     @Test
-    public void getById() {
-        
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-             
-        Stripe.apiKey = "sk_test_51IdfUQGCbrRYfT2gOdvWJFrKI8qU2oZ0jGaahLhiB6ZJ3CrrJzGrhyQmiC1re6I5jd7CwQELcxJFbYv8Si9mn7TF007wjcKqaA"; 
+    public void getById() {        
+      
+        Stripe.apiKey = "(Get code from JMTS)";
         try {
             
-            Product p = Product.retrieve("prod_PebUyutppyNSey");
+            Product p = Product.retrieve("prod_Trh0W06tuvVdLI");
             
             System.out.println("Product: " + p.toJson());
             
--- src/test/java/jm/com/dpbennett/fm/stripe/WebHookAPI.java
+++ src/test/java/jm/com/dpbennett/fm/stripe/WebHookAPI.java
@@ -33,7 +33,7 @@ public class WebHookAPI {
     @Test
     public void getById() {
 
-        Stripe.apiKey = "sk_test_51IdfUQGCbrRYfT2gOdvWJFrKI8qU2oZ0jGaahLhiB6ZJ3CrrJzGrhyQmiC1re6I5jd7CwQELcxJFbYv8Si9mn7TF007wjcKqaA";
+        Stripe.apiKey = "(Get code from JMTS)";
         try {
             Customer cus = Customer.retrieve("cus_Mqg7eRrw2R3Aod");
             System.out.println("Name: " + cus.getName());
